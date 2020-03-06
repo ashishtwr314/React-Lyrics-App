49c60f9e9e9c8e708aa33036af20873c8b6c9a28
Ashish Tiwari <ashishtwr314@gmail.com>
2020-03-06 18:31:33 +0530

    improved UX + Build

--- src/components/Lyrics.jsx
+++ src/components/Lyrics.jsx
@@ -1,79 +1,83 @@
-import React, { Component } from 'react'; 
-import Spinner from './Spinner';
-import Moment from 'react-moment';
+import React, { Component } from "react";
+import Spinner from "./Spinner";
+import Moment from "react-moment";
 
 class Lyrics extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            lyrics : "",
-            track_name: "",
-            artist_name: "",
-            genre: "",
-            album_art: "",
-            rel_date: ""
-         }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      lyrics: "",
+      track_name: "",
+      artist_name: "",
+      genre: "No Genre Specified",
+      album_art: "https://i.ya-webdesign.com/images/cd-album-cover-png-6.png",
+      rel_date: "Released info not available"
+    };
+  }
 
-    componentDidMount = ()=> {
-        fetch(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_API_KEY}`)
-            .then((response) => response.json())
-            .then((responseJson) => {
-                // responseJson.message.body.lyrics.lyrics_body = responseJson.message.body.lyrics.lyrics_body.replace(new RegExp('\r?\n','g'), " ");          
-                this.setState({
-                    lyrics : responseJson.message.body.lyrics.lyrics_body
-                })
-                return fetch(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_API_KEY}`)
-            })
-            .then( (response) => response.json())
-            .then((responseJson)=>{
-                console.log(responseJson);
-                this.setState({
-                    track_name: responseJson.message.body.track.track_name,
-                    artist_name: responseJson.message.body.track.artist_name,
-                    genre: responseJson.message.body.track.primary_genres.music_genre_list[0].music_genre.music_genre_name_extended,
-                    album_art: responseJson.message.body.track.album_coverart_100x100,
-                    rel_date: responseJson.message.body.track.first_release_date
-
-                })
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+  componentDidMount = () => {
+    fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_API_KEY}`
+    )
+      .then(response => response.json())
+      .then(responseJson => {
+        // responseJson.message.body.lyrics.lyrics_body = responseJson.message.body.lyrics.lyrics_body.replace(new RegExp('\r?\n','g'), " ");
+        this.setState({
+          lyrics: responseJson.message.body.lyrics.lyrics_body
+        });
+        return fetch(
+          `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_API_KEY}`
+        );
+      })
+      .then(response => response.json())
+      .then(responseJson => {
+        console.log(responseJson);
+        this.setState({
+          track_name: responseJson.message.body.track.track_name,
+          artist_name: responseJson.message.body.track.artist_name,
+          genre:
+            responseJson.message.body.track.primary_genres.music_genre_list[0]
+              .music_genre.music_genre_name_extended,
+          //   album_art: responseJson.message.body.track.album_coverart_100x100,
+          rel_date: responseJson.message.body.track.first_release_date
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+  render() {
+    if (this.state.lyrics === "") {
+      return <Spinner />;
+    } else {
+      return (
+        <React.Fragment>
+          <div className="row">
+            <div className="col-lg-6 col-sm-6 col-md-6 ">
+              <h3 className="track">{this.state.track_name}</h3>
+              <h4 className="artist">
+                <small>{this.state.artist_name}</small>
+              </h4>
+              <p className="lyrics">{this.state.lyrics}</p>
+            </div>
+            <div className="col-lg-6 col-sm-6 col-md-6 ">
+              <img
+                width="120px"
+                src={this.state.album_art}
+                alt="AlbumArt"
+                className="album-art"
+              />
+              <h3>{this.state.genre}</h3>
+              <br />
+              <h3>
+                <Moment format="DD/MM/YYYY">{this.state.rel_date}</Moment>
+              </h3>
+            </div>
+          </div>
+        </React.Fragment>
+      );
     }
-    render() { 
-    
-
-            if(this.state.lyrics === "")
-            {
-                return <Spinner />
-            }
-            else{
-                return (
-                        <React.Fragment>
-                    
-                            <div className="row">
-                                <div className="col-lg-6 col-sm-6 col-md-6 ">
-                                    <h3 className="track">{this.state.track_name}</h3>
-                                    <h4 className="artist"><small>{this.state.artist_name}</small></h4>
-                                    <p className="lyrics">{this.state.lyrics}</p>
-                                </div>
-                                <div className="col-lg-6 col-sm-6 col-md-6 ">
-                                    <img src={this.state.album_art} alt="AlbumArt" className="album-art" />
-                                    <h3>{this.state.genre}</h3>
-                                    <br /> 
-                                    <h3><Moment format="DD/MM/YYYY">{this.state.rel_date}</Moment></h3>
-                                    
-                                </div>
-                            </div>
-                        </React.Fragment>
-                    
-                )
-                
-            }
-     
-         
-    }
+  }
 }
- 
+
 export default Lyrics;
--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -1,24 +1,28 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Track extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( 
-            <div className="col-lg-6">
-                <div className="tracks-card">
-                    <h4>{this.props.track.track_name}</h4>
-                    <p>{this.props.track.artist_name}</p>
-                    <Link to={`/lyrics/${this.props.track.track_id}`} className="btn btn-block btn-primary">
-                            View Lyrics
-                    </Link>
-                </div>
-            </div>
-         );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    console.log(this.props);
+    return (
+      <div className="col-lg-6">
+        <div className="tracks-card">
+          <h4>{this.props.track.track_name}</h4>
+          <p>{this.props.track.artist_name}</p>
+          <Link
+            to={`/lyrics/${this.props.track.track_id}`}
+            className="btn btn-block btn-primary"
+          >
+            View Lyrics
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Track;
